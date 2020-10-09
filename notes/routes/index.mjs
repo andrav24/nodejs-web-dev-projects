097a7127c268697c4820a516908681c9c5f37d6f
andrav24 <andrav24@yandex.ru>
2020-10-09 19:40:22 +0700

    feat: create basic modules - app,appsupport,approotdir

---
+++ notes/routes/index.mjs
@@ -0,0 +1,7 @@
+import { default as express } from 'express';
+export const router = express.Router();
+
+/* GET home page. */
+router.get('/', async (req, res, next) => {
+  res.render('index', { title: 'Notes' });
+});
